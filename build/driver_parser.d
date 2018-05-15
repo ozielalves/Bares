f0ccfabf897283c93bf4e8d91b723a18ad3e9dbb
Oziel Alves <ozielalves7@gmail.com>
2018-05-15 19:37:11 -0300

    Stack Working

--- build/driver_parser.d
+++
@@ -1,8 +0,0 @@
-build/driver_parser.o: src/driver_parser.cpp src/../include/parser.hpp \
- src/../include/token.hpp src/../include/infix2postfix.hpp
-
-src/../include/parser.hpp:
-
-src/../include/token.hpp:
-
-src/../include/infix2postfix.hpp:
--- build/parser.d
+++
@@ -1,6 +0,0 @@
-build/parser.o: src/parser.cpp src/../include/parser.hpp \
- src/../include/token.hpp
-
-src/../include/parser.hpp:
-
-src/../include/token.hpp:
